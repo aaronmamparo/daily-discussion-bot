23f5a9093794ff8dd351eaaf2158d82aa764a0af
Aaron Mamparo <aaron@aaronmamparo.com>
2017-07-20 18:00:45 -0500

    don't use topics for a year afer used

--- src/bot.py
+++ src/bot.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 			break
 	for topic in topics:
 		r.set(topic, '')
-		r.expire(topic, 2628000)
+		r.expire(topic, 31540000)
 	post_title = 'Daily Discussion %s' % datetime.datetime.now(pytz.timezone('America/Chicago')).strftime('%-m/%-d')
 	post_text = '\n\n&nbsp;\n\n'.join(['%s%s' % ('' if i==0 else ('ALT%s: ' % (i if i>1 else '')), topics[i]) for i in xrange(len(topics))])
 	print '\n' + post_title + '\n----------------------\n'
