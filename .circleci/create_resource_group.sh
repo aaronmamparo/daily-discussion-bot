6954ea7aa37b0ee020ab0d6bebdd2a48c396f12e
Aaron Mamparo <aaron@aaronmamparo.com>
2018-03-11 21:40:30 -0500

    circleci

--- .circleci/create_resource_group.sh
+++ .circleci/create_resource_group.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 RESOURCE_QUERY='{"Type":"TAG_FILTERS_1_0","Query":"{\"ResourceTypeFilters\":[\"AWS::AllSupported\"],\"TagFilters\":[{\"Key\":\"Project\",\"Values\":[\"'"${PROJECT_NAME}"'\"]}]}"}'
-aws resource-groups update-group-query --group-name ${PROJECT_NAME} --resource-query ${RESOURCE_QUERY} ||
-\ aws resource-groups create-group \
+aws resource-groups update-group-query --group-name ${PROJECT_NAME} --resource-query ${RESOURCE_QUERY} || \
+aws resource-groups create-group \
 	--name ${PROJECT_NAME} \
 	--description "${PROJECT_DESCRIPTION}" \
 	--resource-query ${RESOURCE_QUERY} \
