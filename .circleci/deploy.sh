2e48f7d8821c7c1ddf169405961ca157a77b8b30
Aaron Mamparo <aaron@aaronmamparo.com>
2018-03-11 23:59:59 -0500

    circleci

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,23 +4,32 @@ set -eo pipefail
 install_dependencies() {
 	apt-get -y -qq update
 	apt-get -y install python-pip python-dev build-essential
-	pip install awscli awsebcli --upgrade
+	pip install awscli --upgrade
 }
 
 create_resource_group() {
 	RESOURCE_QUERY='{"Type":"TAG_FILTERS_1_0","Query":"{\"ResourceTypeFilters\":[\"AWS::AllSupported\"],\"TagFilters\":[{\"Key\":\"Project\",\"Values\":[\"'"${PROJECT_NAME}"'\"]}]}"}'
-	aws resource-groups update-group-query --group-name ${PROJECT_NAME} --resource-query ${RESOURCE_QUERY} || \
-	aws resource-groups create-group \
-		--name ${PROJECT_NAME} \
-		--description "${PROJECT_DESCRIPTION}" \
-		--resource-query ${RESOURCE_QUERY} \
-		--region "us-east-1"
+	{
+		aws resource-groups update-group-query \
+			--group-name ${PROJECT_NAME} \
+			--resource-query ${RESOURCE_QUERY} \
+			--region "us-east-1"
+	} || {
+		aws resource-groups create-group \
+			--name ${PROJECT_NAME} \
+			--description "${PROJECT_DESCRIPTION}" \
+			--resource-query ${RESOURCE_QUERY} \
+			--region "us-east-1"
+	}
 }
 
 deploy_lambda_function() {
 	aws lambda create-function \
 		--function-name ${PROJECT_NAME} \
-		--runtime python 2.7
+		--role ${LAMBDA_ROLE_ARN} \
+		--handler handler \
+		--runtime python2.7 \
+		--tags Project=${PROJECT_NAME}
 }
 
 install_dependencies
