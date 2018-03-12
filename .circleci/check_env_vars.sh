251281809f40a42a1da9b79be13afc0644b85a59
Aaron Mamparo <aaron@aaronmamparo.com>
2018-03-11 21:31:46 -0500

    circleci

--- .circleci/check_env_vars.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-VALID=1
-if [[ ! -v AWS_ACCESS_KEY_ID ]]; then echo 'Missing environment variable "AWS_ACCESS_KEY_ID"' && VALID=0; fi;
-if [[ ! -v AWS_SECRET_ACCESS_KEY ]]; then echo 'Missing environment variable "AWS_SECRET_ACCESS_KEY"' && VALID=0; fi;
-if [[ ! -v PROJECT_NAME ]]; then echo 'Missing environment variable "PROJECT_NAME"' && VALID=0; fi;
-if [[ ! -v PROJECT_DESCRIPTION ]]; then echo 'Missing environment variable "PROJECT_DESCRIPTION"' && VALID=0; fi;
-if [[ ${VALID} -eq 0 ]]; then exit 1; fi;
--- .circleci/deploy_to_lambda.sh
+++ .circleci/deploy_to_lambda.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
 set -eo pipefail
-
-EB_ENV="${PROJECT_NAME}-prod"
-eb init "${PROJECT_NAME}" -v -p "Docker 17.09.1-ce" -r "us-east-1"
-eb create ${EB_ENV} -v || eb deploy ${EB_ENV} -v
-eb tags ${EB_ENV} -v -a Project=${PROJECT_NAME} || eb tags ${EB_ENV} -v -u Project=${PROJECT_NAME}
-rm -rf .elasticbeanstalk
